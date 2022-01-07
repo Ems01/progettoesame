31268138af0189902369abfed9baea248d2dc1c4
Omar Naja <s1097765@studenti.univpm.it>
2022-01-07 18:00:42 +0100

    Update EnricOmarApplicationTests.java
    Rimosso l'import per asseriEquals, ora non più utilizzato

--- EnricOmar/src/test/java/exam/EnricOmar/EnricOmarApplicationTests.java
+++ EnricOmar/src/test/java/exam/EnricOmar/EnricOmarApplicationTests.java
@@ -1,7 +1,5 @@
 package exam.EnricOmar;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
