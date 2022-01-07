309c1975a8850e824867c6be56d8dfc5953ae324
Omar Naja <s1097765@studenti.univpm.it>
2022-01-07 12:51:41 +0100

    Test1
    Implementato il primo test sulla verifica del corretto funzionamento di "EccezioneGiorno"

--- EnricOmar/src/test/java/exam/EnricOmar/EnricOmarApplicationTests.java
+++ EnricOmar/src/test/java/exam/EnricOmar/EnricOmarApplicationTests.java
@@ -1,13 +1,34 @@
 package exam.EnricOmar;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import project.service.connection;
+
+@SpringBootTest(classes=connection.class)
 class EnricOmarApplicationTests {
+	
+	private connection conn;
+    private String data;
+    
+    @BeforeEach
+    void setUp() {
+    	conn = new connection();
+    }
 
 	@Test
-	void contextLoads() {
+	void TestDataOK() {
+		data = "13.11.2020";
+		equals(conn.getToday(data));
+	}
+	
+	@Test
+	void TestDataNoOK() {
+		data = "13.11.2021";
+		assertEquals("1", conn.getToday(data));
 	}
 
 }
